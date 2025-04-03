6dbd088136e6ae8d2448a07f5bb3a8b4ece4e054
vicky kumar <vicky.kumar@ucd.ie>
2025-04-03 10:29:55 +0100

    basic implemenation of add with comma separted values

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,10 @@
 // stringCalculator.js
 function add(numbers) {
+    //basic implementation
     if (numbers === '') return 0;
     
     const nums = numbers.split(',').map(n => parseInt(n, 10));
+
     return nums.reduce((sum, n) => sum + n, 0);
 }
 
